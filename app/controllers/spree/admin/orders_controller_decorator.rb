b26e191657ba7b20a8dea53393d40ccd66733338
virajrch <viraj@thingbits.net>
2023-03-07 10:13:30 +0530

    Update decorator for Rails 7 Compatibility

--- app/controllers/spree/admin/orders_controller_decorator.rb
+++ app/controllers/spree/admin/orders_controller_decorator.rb
@@ -1,10 +1,15 @@
-Spree::Admin::OrdersController.class_eval do
+module Spree
+  module Admin
+    module OrdersControllerDecorator
+      def self.prepended(base)
+        base.before_action :load_order, only: [:remind]
+      end
 
-  before_action :load_order, only: [:remind]
-
-  def remind
-    Spree::AbandonedCartMailer.reminder_email(@order.id).deliver_later
-    flash[:success] = Spree.t('abandoned_cart_mailer.reminder_email.email_sent')
-    redirect_back(fallback_location: root_path)
+      def remind
+        Spree::AbandonedCartMailer.reminder_email(@order.id).deliver_later
+        flash[:success] = Spree.t('abandoned_cart_mailer.reminder_email.email_sent')
+        redirect_back(fallback_location: root_path)
+      end
+    end
   end
 end
