38335de12584679184428129e13cbdeb2565c516
virajrch <viraj@thingbits.net>
2023-03-07 10:16:51 +0530

    Rails 7 compatibility

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -1,14 +1,19 @@
-Spree::OrdersController.class_eval do
-  before_action :restore_cart, only: :edit
+module Spree
+  module OrdersControllerDecorator
+    def self.prepended(base)
+      base.before_action :restore_cart, only: :edit
+    end
 
-  def restore_cart
-    if params[:cart_token].present?
-      @order = current_order || Spree::Order.incomplete.find_or_initialize_by(token: params[:cart_token])
-      if @order.user
-        redirect_to spree.login_url if spree_current_user.blank?
-      elsif @order.email
-        cookies.signed[:token] = params[:cart_token]
-      end if @order
+    def restore_cart
+      if params[:cart_token].present?
+        @order = current_order || Spree::Order.incomplete.find_or_initialize_by(token: params[:cart_token])
+        if @order.user
+          redirect_to spree.login_url if spree_current_user.blank?
+        elsif @order.email
+          cookies.signed[:token] = params[:cart_token]
+        end if @order
+      end
     end
   end
 end
+Spree::OrdersController.prepend Spree::OrdersControllerDecorator
